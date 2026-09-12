2be4ce9bc1455ab25dc84a0d7b2cc93a17f511c7
Sayali Pawar <sayalipawar1934@gmail.com>
2026-09-12 23:23:15 +0530

    Added pattern problems

---
+++ Patterns/Pattern_1.java
@@ -0,0 +1,15 @@
+public class Pattern_1 {
+    public static void main(String[] args) {
+       //no.of lines
+        for(int i=1; i<=4;i++){
+
+            //prints one line 
+            for(int j=1; j<=4;j++){
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
