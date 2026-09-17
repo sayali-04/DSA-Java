6c035c35c66fe1f58cab89cfb0f7248ce13cefb7
Sayali Pawar <sayalipawar1934@gmail.com>
2026-09-17 23:30:59 +0530

    Added check number is prime or not

---
+++ Maths_Basics/PrimeNo.java
@@ -0,0 +1,17 @@
+public class PrimeNo {
+    public static void main(String[] args) {
+        int n=4;
+        int count=0;
+        for(int i=1; i<=n;i++){  //O(N)
+            if(n%i==0){
+                count++;
+            }
+        }
+            if(count==2){
+                System.out.println("Number is prime");
+            }
+            else{
+                System.out.println("Number is not prime");
+            }
+    }
+}
